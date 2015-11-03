3234a6d026f9c4a22797ac19cc540f0b708ce1fa
LilFaith47 <LilFaith.Llavore@gmail.com>
2015-11-03 14:51:53 -0800

    Javascript update - formdiv

--- HTML/javascript.js
+++ HTML/javascript.js
@@ -30,11 +30,12 @@ window.onload = function () {
             formDiv.style.marginLeft = "auto";
             formDiv.style.marginRight = "auto";
             formDiv.style.height = "1335px";
-            formDiv.style.width = "755px";
+            formDiv.style.width = "750px";
             formDiv.style.position = "absolute";
             formDiv.style.bottom = "0";
-            formDiv.style.top = "10px";
+            formDiv.style.top = "5px";
             formDiv.style.backgroundColor = "#fff";
+            formDiv.style.border = "solid 7px #3E124D";
         
              document.body.appendChild(formDiv);
         
@@ -50,6 +51,13 @@ window.onload = function () {
             fnameInput.style.fontSize="25pt";
             formDiv.appendChild(fnameInput);
         
+            fnameInput.style.marginLeft = "18%";
+            fnameInput.style.marginRight = "15%";
+            fnameInput.style.marginTop = "20px";
+            fnameInput.style.width = "475px";
+            fnameInput.style.height = "50px";
+            fnameInput.style.fontSize = "25pt";
+        
         var lnameInput = document.createElement("input");
             lnameInput.placeholder="Last Name";
             lnameInput.style.display="inline-block";
@@ -62,6 +70,13 @@ window.onload = function () {
             lnameInput.style.border="solid 2px #fff";
             lnameInput.style.fontSize="25pt";
             formDiv.appendChild(lnameInput);
+        
+            lnameInput.style.marginLeft = "18%";
+            lnameInput.style.marginRight = "15%";
+            lnameInput.style.marginTop = "20px";
+            lnameInput.style.width = "475px";
+            lnameInput.style.height = "50px";
+            lnameInput.style.fontSize = "25pt";
         
         var unameInput = document.createElement("input");
             unameInput.placeholder="Username";
@@ -76,6 +91,13 @@ window.onload = function () {
             unameInput.style.fontSize="25pt";
             formDiv.appendChild(unameInput);
         
+            unameInput.style.marginLeft = "18%";
+            unameInput.style.marginRight = "15%";
+            unameInput.style.marginTop = "20px";
+            unameInput.style.width = "475px";
+            unameInput.style.height = "50px";
+            unameInput.style.fontSize = "25pt";
+        
         var emailInput = document.createElement("input");
             emailInput.placeholder="Email";
             emailInput.style.display="inline-block";
@@ -88,6 +110,13 @@ window.onload = function () {
             emailInput.style.border="solid 2px #fff";
             emailInput.style.fontSize="25pt";
             formDiv.appendChild(emailInput);
+        
+            emailInput.style.marginLeft = "18%";
+            emailInput.style.marginRight = "15%";
+            emailInput.style.marginTop = "20px";
+            emailInput.style.width = "475px";
+            emailInput.style.height = "50px";
+            emailInput.style.fontSize = "25pt";
         
         var passInput = document.createElement("input");
             passInput.placeholder="Password";
@@ -102,9 +131,24 @@ window.onload = function () {
             passInput.style.fontSize="25pt";
             formDiv.appendChild(passInput);
         
+            passInput.style.marginLeft = "18%";
+            passInput.style.marginRight = "15%";
+            passInput.style.marginTop = "20px";
+            passInput.style.width = "475px";
+            passInput.style.height = "50px";
+            passInput.style.fontSize = "25pt";
+        
         var subBut = document.createElement("button");
             formDiv.appendChild(subBut);
             subBut.innerHTML = "SIGN UP";
+        
+            subBut.style.marginLeft = "18%";
+            subBut.style.marginRight = "15%";
+            subBut.style.marginTop = "20px";
+            subBut.style.padding = "10px";
+            subBut.style.width = "475px";
+            subBut.style.height = "50px";
+            subBut.style.fontSize = "25pt";
         
             subBut.onclick = function () {
                 document.body.removeChild(formDiv);
