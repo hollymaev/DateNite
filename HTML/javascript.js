368d7f0ddf5e720e1f6cea9c63fee4bea1f32a52
LilFaith47 <LilFaith.Llavore@gmail.com>
2015-11-03 09:39:18 -0800

    Update - V1

---
+++ HTML/javascript.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    var userSignup = document.getElementById ("splashSignup");
+    var userLogin = document.getElementById("splashLogin");
+    var guesLogin = document.getElementById("splashGuest");
+    var buttons = document.getElementById();
+}
