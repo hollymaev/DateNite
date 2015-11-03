d6c6a1eacfae98d7b9aa499a55510f296476b0fb
LilFaith47 <LilFaith.Llavore@gmail.com>
2015-11-03 11:54:37 -0800

    Revert "update v2"
    This reverts commit 23fa33aca61d894eea4e09fa547563573934d01b.

--- HTML/javascript.js
+++ HTML/javascript.js
@@ -1,42 +1,6 @@
 window.onload = function () {
     var userSignup = document.getElementById ("splashSignup");
     var userLogin = document.getElementById("splashLogin");
-    var guestLogin = document.getElementById("splashGuest");
-    var buttons = document.getElementById("splashButton");
-    
-    userSignup.onclick = function () {
-        var formDiv = document.createElement("div");
-        
-            formDiv.style.marginLeft = "150px";
-            formDiv.style.marginRight = "150px";
-            formDiv.style.height = "750px";
-            formDiv.style.width = "auto";
-            formDiv.style.position = "absolute";
-            formDiv.style.right = "0";
-            formDiv.style.left = "0";
-            formDiv.style.top = "5%";
-            formDiv.style.bottom = "0";
-            formDiv.style.backgroundColor = "#fff";
-            formDiv.style.boxShadow = "0 5px 15px black";
-        
-             document.body.appendChild(formDiv);
-        
-        var fnameInput = document.createElement("input");
-            formDiv.appendChild(fnameInput);
-        
-        var lnameInput = document.createElement("input");
-            formDiv.appendChild(lnameInput);
-        
-        var unameInput = document.createElement("input");
-            formDiv.appendChild(unameInput);
-        
-        var emailInput = document.createElement("input");
-            formDiv.appendChild(emailInput);
-        
-        var passInput = document.createElement("input");
-            formDiv.appendChild(passInput);
-        
-        
-               
-    };
-};
+    var guesLogin = document.getElementById("splashGuest");
+    var buttons = document.getElementById();
+}
