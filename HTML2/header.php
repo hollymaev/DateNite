e258397a83b2da74c4b069eba80af429c579aad3
LilFaith47 <LilFaith.Llavore@gmail.com>
2015-11-14 12:58:13 -0800

    Header.php - testing

---
+++ HTML2/header.php
@@ -0,0 +1,14 @@
+<DOCTYPE! HTML>
+<html>
+    <head>
+    <title> Header.php </title>
+    <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="header">
+            <img src="css/images/logo-light.png" id="logo2" alt="LOGO">
+        </div>
+    </body>
+    
+
+</html>
---
+++ HTML2/info2.php
@@ -0,0 +1,14 @@
+<?php
+//this file is for the header to show up on the pages applicable
+include("connection.php");
+?>
+
+<br/>
+
+<?php
+
+include("header.php");
+
+?>
+
+<br/>
