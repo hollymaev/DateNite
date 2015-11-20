db108f734cc195ae2e69ea11f4d52d43246ff2b0
hollymaev <holly_villamin@hotmail.com>
2015-11-20 14:30:13 -0800

    css

--- HTML2/home.php
+++ HTML2/home.php
@@ -17,23 +17,27 @@
             <?php
             
             include("header.php");
+                
 
             ?>
             <div id="blank">
-                <div class="box" id="box1"> TIME </div>
-                <div class="box" id="box2"> BUDGET</div>
+                 <div class="title" id="Choose">  Choose Your  </div>
+                <div class="box" id="box1">  TIME </div>
+                <div class="box" id="box2">  BUDGET</div>
+                
             </div>
             <br/>
             <br/>
             <div id="grey"><img id="optLine" src="css/icon/options-line.png">
-            
+             
             <div class="span" id="ResultSpan">What Do You Think?</div>
+                
         <div id="selectors">
             <div class="selectors" id="budget">
                 <input type="radio" name="budget" id="low" value="Low">
                 <label for="low"><img src="css/icons/budget/icon_budget_l_resize.png"/></label>
                 <input type="radio" name="budget" id="Mid" value="Mid">
-                <label for="Mid"><img src="css/icons/budget/icon_budget_m_resize.png"/></label>
+                <label for="Mid"> <img src="css/icons/budget/icon_budget_m_resize.png"/></label>
                 <input type="radio" name="budget" id="High" value="High">
                 <label for="High"><img src="css/icons/budget/icon_budget_h_resize.png"/></label>
             </div>
@@ -44,8 +48,10 @@
                 <label for="Afternoon"><img src="css/icons/time/icon_time_a2_resize.png"/></label>
                 <input type="radio" name="time" id="Evening" value="Evening">
                 <label for="Evening"><img src="css/icons/time/icon_time_e2_resize.png"/></label>
+                
                 <input type="radio" name="time" id="All" value="All">
-                <label for="All"><img src="css/icons/time/icon_time_ad2_resize.png"/></label>
+                <label for="All"> 
+                    <img src="css/icons/time/icon_time_ad2_resize.png"/></label>
             </div>
         </div>
             
